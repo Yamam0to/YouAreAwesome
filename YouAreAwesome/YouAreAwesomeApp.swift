583027b1756f00468aa679e679a70928982dc2fd
Ajay Yamamoto <atyamamoto226@gmail.com>
2025-11-15 11:36:21 -0700

    Initial Commit

---
+++ YouAreAwesome/YouAreAwesomeApp.swift
@@ -0,0 +1,17 @@
+//
+//  YouAreAwesomeApp.swift
+//  YouAreAwesome
+//
+//  Created by Ajay Yamamoto on 2025-11-15.
+//
+
+import SwiftUI
+
+@main
+struct YouAreAwesomeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
