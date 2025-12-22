8d530658f191efe6cdcb45047e5ca24bf49b1aa0
Ajay Yamamoto <atyamamoto226@gmail.com>
2025-12-21 20:14:48 -0700

    Made images randomly appear while adding a loop to make sure that no image or message runs twice in a row

--- YouAreAwesome/ContentView.swift
+++ YouAreAwesome/ContentView.swift
@@ -10,45 +10,51 @@ import SwiftUI
 struct ContentView: View {
     @State private var message = ""
     @State private var imageName = ""
-    @State private var imageNumber = 0
-    @State private var messagesNumber = 0
+    @State private var lastMessageNumber = -1
+    @State private var lastImageNumber = -1
     var body: some View {
         
         VStack {
-            Spacer()
+            Text(message)
+                .font(.largeTitle)
+                .fontWeight(.heavy)
+                .foregroundStyle(.red)
+                .multilineTextAlignment(.center)
+                .minimumScaleFactor(0.5)
+                .frame(height: 120)
+                .animation(.easeInOut(duration: 0.15), value: message)
             
             Image(imageName)
                 .resizable()
                 .scaledToFit()
                 .clipShape(RoundedRectangle(cornerRadius: 30))
                 .shadow(radius: 30)
-            Text(message)
-                .font(.largeTitle)
-                .fontWeight(.heavy)
-                .foregroundStyle(.red)
-                .multilineTextAlignment(.center)
+                .animation(.default, value: imageName)
+            
             
             Spacer()
             
             Button ("Show Message") {
                 let messages = ["You Are Awesome!",
+                                "Gadzooks my friend! I am astonished at how utterly magnificent you are!",
                                 "Fabulous? That's You!",
                                 "You Swifty!",
                                 "You Are Magic!",
                                 "When the Genius Bar Needs Help, They Call You!"]
-                message = messages[messagesNumber]
-                messagesNumber += 1
                 
-                if messagesNumber == messages.count {
-                    messagesNumber = 0
-                }
+                var messageNumber: Int
+                repeat {
+                    messageNumber = Int.random(in: 0...messages.count - 1)
+                } while messageNumber == lastMessageNumber
+                message = messages[messageNumber]
+                lastMessageNumber = messageNumber
                 
+                var imageNumber: Int
+                repeat {
+                    imageNumber = Int.random(in: 0...9)
+                } while imageNumber == lastImageNumber
                 imageName = "image\(imageNumber)"
-                imageNumber += 1
-                
-                if imageNumber > 9 {
-                    imageNumber = 0
-                }
+                lastImageNumber = imageNumber
                 
             }
             .buttonStyle(.borderedProminent)
