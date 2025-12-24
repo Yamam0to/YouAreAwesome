5741da97b9266abcac711c1d8d72e075ba3b5d60
Ajay Yamamoto <atyamamoto226@gmail.com>
2025-12-24 12:04:25 -0700

    Added random sounds and modified UI

--- YouAreAwesome/ContentView.swift
+++ YouAreAwesome/ContentView.swift
@@ -6,12 +6,17 @@
 //
 
 import SwiftUI
+import AVFAudio
 
 struct ContentView: View {
     @State private var message = ""
     @State private var imageName = ""
     @State private var lastMessageNumber = -1
     @State private var lastImageNumber = -1
+    @State private var lastSoundNumber = -1
+    @State private var audioPlayer: AVAudioPlayer!
+    let numberOfImages = 10
+    let numberOfSounds = 6
     var body: some View {
         
         VStack {
@@ -23,6 +28,8 @@ struct ContentView: View {
                 .minimumScaleFactor(0.5)
                 .frame(height: 120)
                 .animation(.easeInOut(duration: 0.15), value: message)
+            
+            Spacer()
             
             Image(imageName)
                 .resizable()
@@ -42,6 +49,7 @@ struct ContentView: View {
                                 "You Are Magic!",
                                 "When the Genius Bar Needs Help, They Call You!"]
                 
+                //For message
                 var messageNumber: Int
                 repeat {
                     messageNumber = Int.random(in: 0...messages.count - 1)
@@ -49,12 +57,34 @@ struct ContentView: View {
                 message = messages[messageNumber]
                 lastMessageNumber = messageNumber
                 
+                //For image
                 var imageNumber: Int
                 repeat {
-                    imageNumber = Int.random(in: 0...9)
+                    imageNumber = Int.random(in: 0...numberOfImages - 1)
                 } while imageNumber == lastImageNumber
                 imageName = "image\(imageNumber)"
                 lastImageNumber = imageNumber
+                
+                //For sound
+                var soundNumber: Int
+                repeat {
+                    soundNumber = Int.random(in: 0...numberOfSounds - 1)
+                } while soundNumber == lastSoundNumber
+                lastSoundNumber = soundNumber
+                
+                let soundName = "sound\(soundNumber)"
+                guard let soundFile = NSDataAsset(name: soundName) else {
+                    print("🤬 Could not read file named \(soundName)")
+                    return
+                }
+                
+                do {
+                    audioPlayer = try AVAudioPlayer(data: soundFile.data)
+                    audioPlayer.play()
+                } catch {
+                    print("🤬 ERROR: \(error.localizedDescription) creating audioPlayer")
+                }
+                
                 
             }
             .buttonStyle(.borderedProminent)
